298b7c6bf364ef5786d452747e9829c44f0fc117
Yvan <dighan@gmail.com>
2012-04-28 00:46:55 +0200

    Add FPSLocation & FPSInstaller classes

--- fps.py
+++ fps.py
@@ -2,6 +2,17 @@
 
 import optparse
 
+class FPSLocation(object):
+    ARCHIVES = './archives/'
+    PLUGIN_MACOSX = '/Library/Internet Plug-Ins/'
+
+class FPSInstaller(object):
+    def __init__(self, os):
+        self.os = os
+
+    def install(self, version):
+        print version
+
 def main(parser):
     (opts, args) = parser.parse_args()
 
