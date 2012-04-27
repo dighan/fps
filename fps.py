71f1d2f9e35cedfae28eb8aca4970e592a95cdd3
Yvan <dighan@gmail.com>
2012-04-28 01:34:41 +0200

    Use exception to enhance errors management

--- fps.py
+++ fps.py
@@ -1,20 +1,40 @@
 #!/usr/bin/env python
 
-import optparse
+from optparse import OptionParser
 
 class FPSLocation(object):
     ARCHIVES = './archives/'
     PLUGIN_MACOSX = '/Library/Internet Plug-Ins/'
 
+"""
+@todo: manage small & large versions
+       support macosx, win32/64 and linux32/64 bits
+       auto-create default directories if not exist
+       add usage text
+"""
 class FPSInstaller(object):
     def __init__(self, os):
         self.os = os
 
     def install(self, version):
-        print version
+        if not self.has_version(version):
+            raise RuntimeError('No version available')
 
-def main(parser):
-    (opts, args) = parser.parse_args()
+        print self.os + ' ' + version
+
+    def has_version(self, version):
+        return True
+
+def main(options, args):
+    if len(args) != 1:
+        raise RuntimeError('Argument expected : flash player version')
+
+    installer = FPSInstaller('macosx')
+    installer.install(args[0])
 
 if __name__ == '__main__':
-    main(optparse.OptionParser())
+    try:
+        parser = OptionParser()
+        main(*parser.parse_args())
+    except RuntimeError, e:
+        print e
