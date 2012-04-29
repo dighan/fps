097b83ab128d1eda0044b503e7fac78be35f129a
Yvan <dighan@gmail.com>
2012-04-30 00:08:44 +0200

    Manage exceptions by using FPSError class - Optimize code

--- fps.py
+++ fps.py
@@ -6,6 +6,9 @@ import re
 import ConfigParser
 from optparse import OptionParser
 
+class FPSError(Exception):
+    pass
+
 class FPSPath(object):
     BASE = os.getcwd() + os.sep
     CONFIG = BASE + 'config.cfg'
@@ -17,20 +20,11 @@ class FPSInstaller(object):
         self.versions = self.get_versions()
 
     def get_versions(self):
-        versions = []
-
-        for version in os.listdir(FPSPath.ARCHIVES):
-            versions.append(version)
-
-        versions.sort(reverse = True)
-
-        return versions
+        return sorted([version for version in os.listdir(FPSPath.ARCHIVES)], reverse = True)
 
     def parse_version(self, raw_version):
-        matches = re.match('^\d{2}(\.\d(\.\d{3}(\.\d{1,3})?)?)?$', raw_version)
-
-        if matches == None:
-            raise RuntimeError('Invalid pattern.')
+        if not re.match('^\d{2}(\.\d(\.\d{3}(\.\d{1,3})?)?)?$', raw_version):
+            raise FPSError('Invalid version pattern.')
 
         complete_version = None
 
@@ -44,25 +38,23 @@ class FPSInstaller(object):
     def install(self, version):
         complete_version = self.parse_version(version)
 
-        if complete_version == None:
-            raise RuntimeError('No version available.')
+        if not complete_version:
+            raise FPSError('Version %s does not exists' % (version))
 
         if sys.platform != 'darwin':
-            raise RuntimeError('Platform not yet supported (only Mac OS X).')
+            raise FPSError('Current platform is not yet supported (only Mac OS X).')
 
         install_dir = self.config.get(sys.platform, 'install_dir')
 
-        if not os.path.exists(install_dir):
-            os.mkdir(install)
-
-        command = 'sudo cp -r "%s" "%s"' % (FPSPath.ARCHIVES + complete_version + os.sep, install_dir)
-        os.system(command)
+        # create install_dir if not exists
+        # unzip/untar archive file
+        # copy archive content to install_dir (manage permissions)
 
         print 'Flash Player ' + complete_version + ' has been successfully installed !'
 
 def main(options, args):
     if len(args) != 1:
-        raise RuntimeError('Missing argument : version argument expected.')
+        raise FPSError('Missing argument : version is required.')
 
     config = ConfigParser.SafeConfigParser()
     config.read(FPSPath.CONFIG)
@@ -74,5 +66,5 @@ if __name__ == '__main__':
     try:
         parser = OptionParser()
         main(*parser.parse_args())
-    except RuntimeError, exception:
+    except FPSError, exception:
         print exception
